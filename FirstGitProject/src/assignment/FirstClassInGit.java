5da27cd057e50ea874cab12f731efe51adb1ec41
sg16317 <sg16317@CEIT2553214X003.ad.georgiasouthern.edu>
2019-08-21 16:34:25 -0400

    Hello World! added name

--- FirstGitProject/src/assignment/FirstClassInGit.java
+++ FirstGitProject/src/assignment/FirstClassInGit.java
@@ -4,6 +4,7 @@ public class FirstClassInGit {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		System.out.println("My name is Seth!");
 		
 		//add a statement to display your name in the console
 		
